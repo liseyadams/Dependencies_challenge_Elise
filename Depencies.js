e3284d91d43937e3f52508a82dc100ca56f34654
Elise Adams <neuman.elise@gmail.com>
2017-06-29 17:03:40 -0600

    finished result for javascript.

--- Depencies.js
+++ Depencies.js
@@ -1,1 +1,41 @@
-console.log("Hello")
+function depend(input){
+    var packages = []
+
+    var packages_depend = [];
+
+    input.forEach(function(package){
+        package = package.split(": "); 
+        if(package[1] === ""){
+            packages.push(package[0])
+        }else{
+            packages_depend.push({
+                package: package[0],
+                dependency: package[1]
+            })
+        }
+    })
+
+    while(packages_depend.length > 0){
+        var package_count = packages_depend.length;
+
+        packages_depend.forEach(function(with_dependency, index, object){
+            if( packages.indexOf(with_dependency.dependency) >= 0 ){
+                packages.push(with_dependency.package);
+                object.splice(index, 1);
+            }
+        })
+        
+        if(packages_depend.length == package_count){
+            return false;
+        }
+    }
+
+
+    return packages.join(", ")
+}
+
+console.log(depend(['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser: KittenService','Fraudstream: Leetmeme','Ice: ']))
+console.log(depend(['KittenService: ','Leetmeme: Unknown package']))
+console.log(depend(['KittenService: ','Leetmeme: Caramel'])) 
+console.log(depend(['KittenService: ','CamelCaser: KittenService'])) 
+
